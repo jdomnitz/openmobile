50aa4a99cd82de560d6f651ecf7269135bbf42d5
jdomnitz <jdomnitz@23863489-88df-4975-8b6c-3dacf5e6073d>
2010-04-17 18:42:35 +0000

    0.7.0.3 Forgotten Files

---
+++ FrontEnd/OpenMobile/ErrorReporting.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace OpenMobile
+{
+    partial class ErrorReporting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(543, 297);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Restart The Application";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Report this bug to the developers";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ErrorReporting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 340);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ErrorReporting";
+            this.Text = "OpenMobile Error Reporting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ FrontEnd/OpenMobile/ErrorReporting.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace OpenMobile
+{
+    public partial class ErrorReporting : Form
+    {
+        public ErrorReporting(string text)
+        {
+            InitializeComponent();
+            textBox1.Text = text;
+            button1.Select();
+            try
+            {
+                Clipboard.SetText(text);
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result=HttpPost("http://openmobile.sourceforge.net/main/BugReports/index.php","REPORT="+textBox1.Text.Replace("\r\n","|"));
+            if (result == "File saved")
+                MessageBox.Show("Report Submitted Successfully!  Thank you for helping to improve openMobile.");
+            else
+                MessageBox.Show("Unable to submit error report");
+        }
+
+        string HttpPost (string uri, string parameters)
+        { 
+           // parameters: name1=value1&name2=value2	
+           WebRequest webRequest = WebRequest.Create (uri);
+           //string ProxyString = 
+           //   System.Configuration.ConfigurationManager.AppSettings
+           //   [GetConfigKey("proxy")];
+           //webRequest.Proxy = new WebProxy (ProxyString, true);
+           //Commenting out above required change to App.Config
+           webRequest.ContentType = "application/x-www-form-urlencoded";
+           webRequest.Method = "POST";
+           byte[] bytes = Encoding.ASCII.GetBytes (parameters);
+           Stream os = null;
+           try
+           { // send the Post
+              webRequest.ContentLength = bytes.Length;   //Count bytes to send
+              os = webRequest.GetRequestStream();
+              os.Write (bytes, 0, bytes.Length);         //Send it
+           }
+           catch (WebException ex)
+           {
+               return null;
+           }
+           finally
+           {
+              if (os != null)
+              {
+                 os.Close();
+              }
+           }
+         
+           try
+           { // get the response
+              WebResponse webResponse = webRequest.GetResponse();
+              if (webResponse == null) 
+                 { return null; }
+              StreamReader sr = new StreamReader (webResponse.GetResponseStream());
+              return sr.ReadToEnd ().Trim ();
+           }
+           catch (WebException ex)
+           {
+               return null;
+           }
+           return null;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            button3.Select();
+        }
+    }
+}
