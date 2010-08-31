985aa17c81860cf35cdd2d743ae98cc1ee838536
jdomnitz <jdomnitz@23863489-88df-4975-8b6c-3dacf5e6073d>
2010-08-31 06:46:27 +0000

    * ADDED: OMFolderDB * BUGFIX: First character typed in a password box was escaped * BUGFIX: Resizing less abusive to the CPU (still needs some work)

---
+++ Plugins/OMFolderDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OMFolderDB")]
+[assembly: AssemblyDescription("Open Mobile Folder Database Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Open Mobile")]
+[assembly: AssemblyProduct("Open Mobile")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("Protected by GPLv3")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5326e32-233f-4dce-afbf-6f98c0fcf960")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
