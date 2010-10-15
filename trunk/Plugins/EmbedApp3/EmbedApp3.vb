8ae698a327116d91be8c107a39407d39e037bb56
jheizer <jheizer@23863489-88df-4975-8b6c-3dacf5e6073d>
2010-10-15 01:11:54 +0000

    ADDED: EmbedApp1, EmbedApp2, EmbedApp3

--- trunk/Plugins/EmbedApp3/EmbedApp3.vb
+++ trunk/Plugins/EmbedApp3/EmbedApp3.vb
@@ -1,10 +1,29 @@
-﻿Imports OpenMobile
+﻿#Region "GPL"
+'    This file is part of OpenMobile.
+
+'    OpenMobile is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+
+'    OpenMobile is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+
+'    You should have received a copy of the GNU General Public License
+'    along with OpenMobile.  If not, see <http://www.gnu.org/licenses/>.
+
+'    Copyright 2010 Jonathan Heizer jheizer@gmail.com
+#End Region
+
+Imports OpenMobile
 Imports OpenMobile.Controls
 Imports OpenMobile.Plugin
 Imports OpenMobile.Framework
 Imports System.Runtime.InteropServices
 
-Public Class ExternalNav
+Public Class EmbedApp3
     Implements IHighLevel
 
     Private WithEvents m_Host As IPluginHost
@@ -17,12 +36,16 @@ Public Class ExternalNav
     Private m_WindowName As String
     Private m_Process As Process
 
+    Private m_EmbedAppString As String = "EmbedApp3"
+
+    Private m_DisplayName As String = m_EmbedAppString
+
     Private Sub m_Host_OnSystemEvent(ByVal funct As OpenMobile.eFunction, ByVal arg1 As String, ByVal arg2 As String, ByVal arg3 As String) Handles m_Host.OnSystemEvent
         If Not String.IsNullOrEmpty(m_Exe) Then
             Select Case funct
                 Case Is = eFunction.TransitionToPanel
                     If Not m_Embedded Then
-                        If arg2 = "ExternalNav" Then
+                        If arg2 = m_EmbedAppString Then
                             If m_Process Is Nothing Then
                                 LoadNavApp()
                             End If
@@ -63,11 +86,13 @@ Public Class ExternalNav
     Public Function initialize(ByVal host As OpenMobile.Plugin.IPluginHost) As OpenMobile.eLoadStatus Implements OpenMobile.Plugin.IBasePlugin.initialize
         m_Host = host
         LoadNavSettings()
+
+        Return eLoadStatus.LoadSuccessful
     End Function
 
     Private Sub LoadNavApp()
         m_Process = Process.Start(m_Exe)
-        'Give it some time to load before moving it
+        'Give it some time to load before embedding it
         System.Threading.Thread.Sleep(m_Delay * 1000)
     End Sub
 
@@ -75,13 +100,14 @@ Public Class ExternalNav
         If m_Settings Is Nothing Then
             LoadNavSettings()
 
-            m_Settings = New Settings("External GPS")
-            m_Settings.Add(New Setting(SettingTypes.File, "ExternalNav.Exe", "EXE", "App EXE", m_Exe))
+            m_Settings = New Settings(m_EmbedAppString)
+            m_Settings.Add(New Setting(SettingTypes.File, m_EmbedAppString & ".Exe", "EXE", "App EXE", m_Exe))
             Dim Range As New Generic.List(Of String)
             Range.Add("1")
             Range.Add("20")
-            m_Settings.Add(New Setting(SettingTypes.Range, "ExternalNav.Delay", "Delay", "App Start Up Delay (1-20 Seconds)", Nothing, Range, m_Delay))
-           
+            m_Settings.Add(New Setting(SettingTypes.Range, m_EmbedAppString & ".Delay", "Delay", "App Start Up Delay (1-20 Seconds)", Nothing, Range, m_Delay))
+            m_Settings.Add(New Setting(SettingTypes.Text, m_EmbedAppString & ".DisplayName", "", "Main Menu Text", m_DisplayName))
+
             AddHandler m_Settings.OnSettingChanged, AddressOf Changed
         End If
 
@@ -89,24 +115,39 @@ Public Class ExternalNav
     End Function
 
     Private Sub Changed(ByVal St As Setting)
-        Dim PlugSet As New OpenMobile.Data.PluginSettings
-        PlugSet.setSetting(St.Name, St.Value)
+        Using PlugSet As New OpenMobile.Data.PluginSettings
+
+            PlugSet.setSetting(St.Name, St.Value)
+
+            If St.Name = m_EmbedAppString & ".DisplayName" Then
+                For i As Integer = 1 To 3
+                    For j As Integer = 1 To 3
+                        If PlugSet.getSetting("MainMenu.MainMenu" & i & j & ".Plugin") = m_EmbedAppString Then
+                            PlugSet.setSetting("MainMenu.MainMenu" & i & j & ".Display", St.Value)
+                        End If
+                    Next
+                Next
+            End If
+
+        End Using
     End Sub
 
     Private Sub LoadNavSettings()
         Using Settings As New OpenMobile.Data.PluginSettings
-            If String.IsNullOrEmpty(Settings.getSetting("ExternalNav.Exe")) Then
+            If String.IsNullOrEmpty(Settings.getSetting(m_EmbedAppString & ".Exe")) Then
                 CreateSettings()
             End If
-            m_Exe = Settings.getSetting("ExternalNav.Exe")
-            m_Delay = Settings.getSetting("ExternalNav.Delay")
+            m_Exe = Settings.getSetting(m_EmbedAppString & ".Exe")
+            m_Delay = Settings.getSetting(m_EmbedAppString & ".Delay")
+            m_DisplayName = Settings.getSetting(m_EmbedAppString & ".DisplayName")
         End Using
     End Sub
 
     Private Sub CreateSettings()
         Using Settings As New OpenMobile.Data.PluginSettings
-            Settings.setSetting("ExternalNav.Exe", "")
-            Settings.setSetting("ExternalNav.Delay", "8")
+            Settings.setSetting(m_EmbedAppString & ".Exe", "")
+            Settings.setSetting(m_EmbedAppString & ".Delay", "8")
+            Settings.setSetting(m_EmbedAppString & ".DisplayName", m_EmbedAppString)
         End Using
     End Sub
 
@@ -124,13 +165,13 @@ Public Class ExternalNav
 
     Public ReadOnly Property pluginDescription() As String Implements OpenMobile.Plugin.IBasePlugin.pluginDescription
         Get
-            Return "Embed External Navigation"
+            Return m_EmbedAppString
         End Get
     End Property
 
     Public ReadOnly Property pluginName() As String Implements OpenMobile.Plugin.IBasePlugin.pluginName
         Get
-            Return "ExternalNav"
+            Return m_EmbedAppString
         End Get
     End Property
 
@@ -142,7 +183,8 @@ Public Class ExternalNav
 
     Public ReadOnly Property displayName() As String Implements OpenMobile.Plugin.IHighLevel.displayName
         Get
-            Return "GPS"
+            LoadNavSettings()
+            Return m_DisplayName
         End Get
     End Property
 
@@ -176,5 +218,4 @@ Public Class ExternalNav
         GC.SuppressFinalize(Me)
     End Sub
 #End Region
-
 End Class
