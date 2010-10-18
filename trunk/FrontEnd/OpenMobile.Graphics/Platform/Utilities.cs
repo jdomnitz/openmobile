b2bcc2d4b28dec81d0aacc98f2d353eb9e8e93bd
jdomnitz <jdomnitz@23863489-88df-4975-8b6c-3dacf5e6073d>
2010-10-18 16:33:32 +0000

    Sync with OpenTK trunk (fixes show/hide cursor functions on linux)

--- trunk/FrontEnd/OpenMobile.Graphics/Platform/Utilities.cs
+++ trunk/FrontEnd/OpenMobile.Graphics/Platform/Utilities.cs
@@ -172,28 +172,6 @@ namespace OpenMobile.Platform
 
         #region --- Creating a Graphics Context ---
 
-        /// <summary>
-        /// Creates an IGraphicsContext instance for the specified window.
-        /// </summary>
-        /// <param name="mode">The GraphicsMode for the GraphicsContext.</param>
-        /// <param name="window">An IWindowInfo instance describing the parent window for this IGraphicsContext.</param>
-        /// <param name="major">The major OpenGL version number for this IGraphicsContext.</param>
-        /// <param name="minor">The minor OpenGL version number for this IGraphicsContext.</param>
-        /// <param name="flags">A bitwise collection of GraphicsContextFlags with specific options for this IGraphicsContext.</param>
-        /// <returns>A new IGraphicsContext instance.</returns>
-        [Obsolete("Call new OpenMobile.Graphics.GraphicsContext() directly, instead.")]
-        public static IGraphicsContext CreateGraphicsContext(
-            GraphicsMode mode, IWindowInfo window,
-            int major, int minor, GraphicsContextFlags flags)
-        {
-            GraphicsContext context = new GraphicsContext(mode, window, major, minor, flags);
-            context.MakeCurrent(window);
-
-            (context as IGraphicsContextInternal).LoadAll();
-
-            return context;
-        }
-
         #region CreateX11WindowInfo
 
         /// <summary>
