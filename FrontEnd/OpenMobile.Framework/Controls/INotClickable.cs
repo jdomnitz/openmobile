33122efa57f3b8afc56db1db0681ee905da7491c
jdomnitz <jdomnitz@23863489-88df-4975-8b6c-3dacf5e6073d>
2010-12-06 09:20:29 +0000

    CHANGE: Renamed DirectShowLib to WindowsAudio CHANGE: Renamed MediaFoundation to VistaAudio ADDED:  INotClickable interface to allow click through regions ADDED:  CopyPixels and PastePixels graphics functions

---
+++ FrontEnd/OpenMobile.Framework/Controls/INotClickable.cs
@@ -0,0 +1,18 @@
+﻿
+namespace OpenMobile.Controls
+{
+    /// <summary>
+    /// Any control implementing this interface will not be clickable
+    /// </summary>
+    public interface INotClickable
+    {
+        /// <summary>
+        /// Return true if the point should be clickable
+        /// Return false if the click should fall through
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        bool IsPointClickable(int x, int y);
+    }
+}
