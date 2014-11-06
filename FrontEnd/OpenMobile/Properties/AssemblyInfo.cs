2b957fb40617d25f47299c2406f52deab74980b9
boorte <boorte@23863489-88df-4975-8b6c-3dacf5e6073d>
2014-11-06 22:20:49 +0000

    [NEW] Tag reader now uses filename if Mp3 tag is empty. [REMOVED] Removed references to HAL interface

--- FrontEnd/OpenMobile/Properties/AssemblyInfo.cs
+++ FrontEnd/OpenMobile/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.2014.1030")]
+[assembly: AssemblyFileVersion("0.9.2014.1106")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
